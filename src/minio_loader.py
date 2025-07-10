94790f3f838923dfc91d74af41952c86384f6ea7
Новиков Илья <ianovikov@lanit.ru>
2025-07-11 00:46:13 +0300

    Revert "fix using queue type for thread queue"
    This reverts commit dc60b6255fd913527891d804b0897a122fe50e64.

--- src/minio_loader.py
+++ src/minio_loader.py
@@ -7,7 +7,6 @@ import traceback
 from concurrent.futures import Future, ThreadPoolExecutor
 from datetime import datetime
 from multiprocessing import Queue
-from queue import Queue as TQueue
 
 import pandas as pd
 from botocore.exceptions import ClientError
@@ -41,7 +40,7 @@ class LoadClusterTask:
         bucket_name: str,
         cluster_name: str,
         q_manager: QueueManager,
-        report_queue: "TQueue[tuple[TaskID, bool]]",
+        report_queue: "Queue[tuple[TaskID, bool]]",
         stop_event: Event,
         start_time: datetime,
         end_time: datetime,
@@ -69,7 +68,7 @@ class LoadClusterTask:
         self._q_manager: QueueManager = q_manager
         self._processor_queue: "Queue[LoadComplete]"
         self._stop_event: Event = stop_event
-        self._report_queue: "TQueue[tuple[TaskID, bool]]" = report_queue
+        self._report_queue: "Queue[tuple[TaskID, bool]]" = report_queue
 
     def __call__(self):
         """ "
